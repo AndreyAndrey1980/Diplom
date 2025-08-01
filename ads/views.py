b3c1ede72d343620fb098bc7d2fdedb9bcde4dc6
andrey19801308 <mandibul1@mail.ru>
2025-08-01 10:09:31 +0300

    Исправил

--- ads/views.py
+++ ads/views.py
@@ -10,7 +10,6 @@ from .pagination import StandardResultsSetPagination
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrAdmin]
 
     def get_queryset(self):
         return Comment.objects.filter(ad_id=self.kwargs['ad_pk'])
@@ -18,14 +17,30 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, ad_id=self.kwargs['ad_pk'])
 
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [permissions.AllowAny()]
+        elif self.action in ['create']:
+            return [permissions.IsAuthenticated()]
+        else:  # update, partial_update, destroy
+            return [IsAuthorOrAdmin()]
+
 
 class AdViewSet(viewsets.ModelViewSet):
     queryset = Ad.objects.all()
     serializer_class = AdSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ['title']
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrAdmin]
     pagination_class = StandardResultsSetPagination
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [permissions.AllowAny()]
+        elif self.action in ['create']:
+            return [permissions.IsAuthenticated()]
+        else:  # update, partial_update, destroy
+            return [IsAuthorOrAdmin()]
+
